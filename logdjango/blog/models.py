84622d41a058e0eeaec73f7802bcd87017b7dd2f
Khanafin Timur <mindiisky@yandex.ru>
2023-11-20 16:25:02 +0300

    views

--- logdjango/blog/models.py
+++ logdjango/blog/models.py
@@ -3,20 +3,20 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 class Post(models.Model):
     class PostStatus(models.TextChoices):
         DRAFT = "draft", _("Draft")
         PUBLISHED = "published", _("Pusblished")
 
     title = models.CharField(max_length=250, verbose_name="Заголовок")
-    slug = models.SlugField(max_length= 250, unique_for_date='publish')
-    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
+    slug = models.SlugField(max_length= 250, unique_for_date='publish', verbose_name="Опубликовано")
+    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE, verbose_name="Автор")
     body = models.TextField()
     publish = models.DateTimeField(default= timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=PostStatus.choices, default=PostStatus.DRAFT)
+    status = models.CharField(max_length=10, choices=PostStatus.choices, default=PostStatus.DRAFT, verbose_name="Статус")
 
     class Meta:
         ordering = ('-publish',)
